7979e043f3e6d87a4fcf6291aebebb00205bf5f6
Sam Zagrobelny <strangesast@gmail.com>
2014-10-25 19:31:02 -0400

    added basic port test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var SerialPort = require("serialport");
+
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.post('/', function(req, res) {
+	var data = req.body;
+	var method = data.method;
+	if(data.method == 'test') {
+		SerialPort.list(function(err, ports) {
+			cb(ports);
+		});
+	}
 
+	function cb(obj) {
+		res.send(obj);
+	}
+	
 });
 
 module.exports = router;
