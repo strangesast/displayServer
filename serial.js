752c172edfba574745c6b0b5d1c6453531e78e42
Sam Zagrobelny <strangesast@gmail.com>
2014-10-25 20:54:16 -0400

    teensy identified, location returned

--- serial.js
+++ serial.js
@@ -1,8 +1,23 @@
 var SerialPort = require("serialport");
 
 function testSerial(callback) {
+	// return location of first Teensy device
+	function cb(data) {
+		if(data.length) {
+		  for(var i=0; i<data.length; i++) {
+				var _location = data[i].comName;
+				var _name = data[i].pnpId;
+				if(_name.substring(4, 10) == "Teensy") {
+					callback(_location);
+					break;
+				}
+			}	
+		} else {
+			callback('noDeviceConnected');
+		}
+	}
   SerialPort.list(function(err, ports) {
-  	callback(ports);
+  	cb(ports);
 	});
 }
 
