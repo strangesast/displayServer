ec633f1269a408ce2d0bc553b300c14126480222
Sam Zagrobelny <strangesast@gmail.com>
2014-12-02 23:00:01 +0000

    shapes can be updated

--- routes/index.js
+++ routes/index.js
@@ -21,18 +21,31 @@ function getLocations(callback) {
 
 var busy;
 
-function writeString(callback, serialPortObject, string) {
+function writeString(callback, serialPortObject, string, clear) {
+	var clearStr = "<x0|y0|w120|h8|sW100,000000|tW|b70|>";
 	busy = true
+	function clearIt(bool, callback) {
+		if(bool) {
+		  serialPortObject.write(clearStr, 
+														 function(err, results) {callback();});
+	  } else {
+			console.log('toast');
+			callback();
+		}
+	}
+
 	serialPortObject.open(function(err) {
 		if(!err) {
 			serialPortObject.on('open', function() {
-				port.write(string, function(err, results) {
-					if(!err) {
-						callback('write success');
-					} else {
-						callback('error writing to serial');
-					}
-				})
+				clearIt(true, function() {
+				  serialPortObject.write(string, function(err, results) {
+				  	if(!err) {
+				  		callback('write success');
+				  	} else {
+				  		callback('error writing to serial');
+				  	}
+				  });
+				});
 
 			});
 
@@ -44,7 +57,6 @@ function writeString(callback, serialPortObject, string) {
 }
 
 
-/* GET home page. */
 function test(callback, color) {
 	getLocations(function(ports) {
 		for(var i=0; i<ports.length; i++) {
@@ -53,7 +65,7 @@ function test(callback, color) {
   		port = new SerialPort.SerialPort(ports[i].location, {baudrate:9600});
 			var str = "<x0|y0|w100|v8|sW10," + color.substring(0, 6) + "|tW|b70|>"
 			if(!busy){
-				writeString(callback, port, str);
+				writeString(callback, port, str, true);
 			} else {
 				callback('busy');
 			}
@@ -61,7 +73,26 @@ function test(callback, color) {
 	});
 }
 
-var routing = {'test': test};
+function update(callback, obj) {
+	var _x = obj.x;
+	var _y = obj.y;
+	var _w = obj.w + _x;
+	var _h = obj.h + _y;
+	var str = "<x" + _x + "|y" + _y + "|w" + _w + "|v" + _h + "|" + "sW10,FF0000|tW|b70|>";
+	console.log(str);
+	getLocations(function(ports) {
+	  for(var i=0; i<ports.length; i++){
+	  	writeString(
+	  		function(_obj) {console.log(_obj)},
+	  	 	new SerialPort.SerialPort(ports[i].location, {baudrate:9600}),
+	  		str
+	  	);
+	  }
+	  callback('gotit')
+	});
+}
+
+var routing = {'test': test, 'update': update};
 
 router.post('/', function(req, res) {
 	var data = req.body;
@@ -74,10 +105,6 @@ router.post('/', function(req, res) {
 	
 });
 
-<<<<<<< HEAD
-
-=======
->>>>>>> b4c53b125797611888b2904dce4f48b43de0388f
 router.get('/', function(req, res) {
 	res.render('index');
 });
