0d7e0dda9d25a1544a7e764b1cab18fb410237d2
Sam Zagrobelny <strangesast@gmail.com>
2015-02-05 11:53:21 -0500

    added serialport send structure

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,34 @@ var serialPort = require('serialport');
 var config = require('../config');
 
 var Ports;
+var openPorts = {};
 
+// return serialized version of display object
+function serializeObject(_object) {
+	// _object (Object)
+	return '';
+}
+
+function returnPort(_comName) {
+	// _comName (String)
+
+}
+
+function sendSerial(_port, _buffer) {
+	// _port (Promise), _buffer (Buffer)
+	return new Promise(function(resolve, reject) {
+		_port.then(function(p) {
+			// may need to add flush
+			p.open(function(err) {
+				// may need to wait for open event
+				p.write(_buffer, function(err) {
+					if(err) reject(err);
+					else resolve();
+				});
+			});
+		});
+	});
+}
 
 function refreshAvailablePorts() {
 	return new Promise(function(resolve, reject) {
