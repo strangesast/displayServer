aab88fab45f35956c25994a89f35e2eaadf7f1f8
Sam Zagrobelny <strangesast@gmail.com>
2015-04-07 13:13:19 -0400

    added preliminary connection page

---
+++ routes/connections.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var config = require('../config');
+var Promise = require('es6-promise').Promise;
+var net = require('net');
+var JsonSocket = require('json-socket');
+
+var scoreboard = new JsonSocket(new net.Socket());
+
+router.route('/')
+.get(function(req, res, next) {
+	res.render('connections', {'title' : 'Connections'});
+
+})
+.post(function(req, res, next) {
+	var body = req.body;
+	console.log(body);
+	if('connection' in body) {
+		var con = body.connection;
+		var = con.host;
+	  var = con.port;
+		initiateConnection(host, port).then(function(connection) {
+			connection.on('message', function(message) {
+				console.log(message);
+			});
+
+		}, function(err) {
+			return next(new Error(err));
+		});
+	}
+})
+
+
+var initiateConnection = function(host, port) {
+	if(!net.isIP(host)) return Promise.reject('bad ip');
+	return new Promise(function(resolve, reject) {
+	  scoreboard.connect(port, host)
+		resolve(scoreboard);
+	})
+
+}
+
+module.exports = router;
