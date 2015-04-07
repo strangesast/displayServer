cddaccb6ddb67ea6720d46aa54f9a68017c90876
Sam Zagrobelny <strangesast@gmail.com>
2015-04-07 14:25:53 -0400

    messaging working

--- public/javascripts/connections.js
+++ public/javascripts/connections.js
@@ -1,1 +1,32 @@
- 
+var submit = function() {
+  var host = document.getElementById('host').value;
+  var port = document.getElementById('port').value;
+  var obj = {
+    connection : {
+      host: host,
+      port: port
+    }
+  }
+  $.ajax({
+    type: 'POST',
+    url:  '/connections',
+    data: JSON.stringify(obj),
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json'
+  }).done(function(data) {
+    console.log(data);
+  }).fail(function(err) {
+    console.log(err);
+  })
+} 
+
+
+var loc = window.location.href;
+var full = 'ws://' + loc + '/';
+var full = 'ws://127.0.0.1:8083';
+
+var socket = new WebSocket(full);
+
+socket.onmessage = function(message) {
+  console.log(message);
+}
--- routes/connections.js
+++ routes/connections.js
@@ -5,7 +5,23 @@ var Promise = require('es6-promise').Promise;
 var net = require('net');
 var JsonSocket = require('json-socket');
 
-var scoreboard = new JsonSocket(new net.Socket());
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({ port: 8083 });
+
+
+wss.on('connection', function(ws) {
+  ws.on('message', function(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('welcome');
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
 
 router.route('/')
 .get(function(req, res, next) {
@@ -14,28 +30,44 @@ router.route('/')
 })
 .post(function(req, res, next) {
 	var body = req.body;
-	console.log(body);
 	if('connection' in body) {
 		var con = body.connection;
 		var host = con.host;
 	  var port = con.port;
 		initiateConnection(host, port).then(function(connection) {
-			connection.on('message', function(message) {
-				console.log(message);
+      console.log('here');
+			connection.on('error', function(err) {
+        console.log('err');
+        wss.broadcast(JSON.stringify(err));
 			});
+      connection.on('message', function(message) {
+        console.log('message');
+        wss.broadcast(JSON.stringify(message));
+      });
 
+      connection.on('connect', function(message) {
+        console.log('connect');
+        wss.broadcast(JSON.stringify(message));
+      });
+
+      res.json();
 		}, function(err) {
-			return next(new Error(err));
+			res.status(400).json(err);
 		});
-	}
-})
+	} else {
+    res.json('toast');
+  }
+});
 
 
 var initiateConnection = function(host, port) {
-	if(!net.isIP(host)) return Promise.reject('bad ip');
 	return new Promise(function(resolve, reject) {
-	  scoreboard.connect(port, host)
-		resolve(scoreboard);
+    console.log('attempting to connect on ' + host + ' ' + port);
+    var connection = net.createConnection({host: host, port: port}, function(err, socket) {
+      console.log(socket);
+    });
+
+		resolve(connection);
 	})
 
 }
